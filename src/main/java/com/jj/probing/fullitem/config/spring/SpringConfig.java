c0db2d8056d37c6bcdec1f1de8403b8be7d1e9fc
Juan Jose Barrera <barrerajuanjose@gmail.com>
2020-09-20 11:10:30 -0300

    remove book

--- src/main/java/com/jj/probing/fullitem/config/spring/SpringConfig.java
+++ src/main/java/com/jj/probing/fullitem/config/spring/SpringConfig.java
@@ -3,8 +3,6 @@ package com.jj.probing.fullitem.config.spring;
 import com.jj.probing.fullitem.repositories.rest.ItemRestRepository;
 import com.jj.probing.fullitem.usecases.FindItem;
 import com.jj.probing.fullitem.usecases.port.ItemRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
 
 public class SpringConfig {
     public FindItem createFindItem() {
